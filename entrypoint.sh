397611097bace18f6fc8064a7ac2b1dcaff3fd2f
Jaume Arús <jaume.arus@paack.co>
2019-12-20 14:56:41 +0100

    Add postgis extension

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -e POSTGRES_DB=$INPUT_POSTGRESQL_DB"
+docker_run="$docker_run -e POSTGRES_DBNAME=$INPUT_POSTGRESQL_DB"
 docker_run="$docker_run -e POSTGRES_USER=$INPUT_POSTGRESQL_USER"
-docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
+docker_run="$docker_run -e POSTGRES_PASS=$INPUT_POSTGRESQL_PASSWORD"
+docker_run="$docker_run -e POSTGRES_MULTIPLE_EXTENSIONS=postgis"
 docker_run="$docker_run -d -p 5432:5432 kartoza/postgis:$INPUT_POSTGRESQL_VERSION"
+
+echo $docker_run
 
 sh -c "$docker_run"
